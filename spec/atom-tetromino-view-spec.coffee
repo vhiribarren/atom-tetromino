c244827b44effe6b36ea05286aaf043aa6beb43d
vhiribarren <vhiribarren@users.noreply.github.com>
2015-12-27 18:02:49 +0100

    Mod: converted some coffee files to javascript

--- spec/atom-tetromino-view-spec.coffee
+++
@@ -1,5 +0,0 @@
-AtomTetrominoView = require '../lib/atom-tetromino-view'
-
-describe "AtomTetrominoView", ->
-  it "has one valid test", ->
-    expect("life").toBe "easy"
---
+++ spec/atom-tetromino-view-spec.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const AtomTetrominoView = require('../lib/atom-tetromino-view');
+
+describe("AtomTetrominoView", () => {
+  it("has one valid test", () => {
+    expect("life").toBe("easy");
+  });
+});
