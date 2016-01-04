30cfab2d2e00824cc8a9f1f9a2e9a47e31489213
vhiribarren <vhiribarren@users.noreply.github.com>
2016-01-04 13:27:54 +0000

    Del: removed spec files, not used for now

--- spec/tetromino-view-spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const AtomTetrominoView = require('../lib/atom-tetromino-view');
-
-describe("AtomTetrominoView", () => {
-  it("has one valid test", () => {
-    expect("life").toBe("easy");
-  });
-});
